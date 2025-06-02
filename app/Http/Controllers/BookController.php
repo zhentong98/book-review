0353878a239cc0408ebdb6ab7628b01a176d7d3f
zhentong98 <zhentong98@hotmail.com>
2025-06-02 19:34:20 +0800

    Update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -25,11 +25,17 @@ class BookController extends Controller
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
-
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
 
         $books = $books->get();
+
+//        $cacheKey = 'books:' . $filter . ':' . $title;
+//        $books = cache()->remember(
+//            $cacheKey,
+//            3600,
+//            fn() => $books->get()
+//        );
 
         return view('books.index', ['books' => $books]);
     }
@@ -53,9 +59,19 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $cacheKey = 'book:' . $id;
+
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn() => Book::with([
+                'reviews' => fn($query) => $query->latest()
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
+        );
+
+        return view('books.show', ['book' => $book,]);
     }
 
     /**
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * 
+ *
  *
  * @property int $id
  * @property string $review
@@ -36,5 +36,11 @@ class Review extends Model
     public function book()
     {
         return $this->belongsTo(Book::class);
+    }
+
+    protected static function booted()
+    {
+        static::updated(fn(Review $review) => cache()->forget('book:' . $review->book_id));
+        static::deleted(fn(Review $review) => cache()->forget('book:' . $review->book_id));
     }
 }
