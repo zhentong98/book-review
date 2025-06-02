97a282527da89f3d7349dc20ab97f7666b0fa4e5
zhentong98 <zhentong98@hotmail.com>
2025-06-02 17:36:12 +0800

    popular or highest rated filter.

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -6,17 +6,38 @@
     </h1>
 
     <form method="GET" action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
-        <input class="input h-10" type="text" name="title" placeholder="Search by title" value="{{ request('title') }}"/>
+        <input class="input h-10" type="text" name="title" placeholder="Search by title"
+               value="{{ request('title') }}"/>
+        <input type="hidden" name="filter" value="{{ request('filter') }}"/>
         <button type="submit" class="btn h-10">Search</button>
         <a href="{{route('books.index')}}" class="btn h-10">Clear</a>
     </form>
+
+    <div class="filter-container mb-4 flex">
+        @php
+            $filters = [
+            '' => 'Latest',
+            'popular_last_month' => 'Popular Last Month',
+            'popular_last_6month' => 'Popular Last 6 Months',
+            'highest_rated_last_month' => 'Highest Rated Last Month',
+            'highest_rated_last_6month' => 'Highest Rated Last 6 Months',
+            ];
+        @endphp
+
+        @foreach($filters as $key => $label)
+            <a href="{{route('books.index', [...request()->query(),'filter' => $key])}}"
+               class="{{request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item'}}">
+                {{ $label  }}
+            </a>
+        @endforeach
+    </div>
 
     <ul>
         @forelse($books as $book)
             <li class="mb-4">
                 <div class="book-item">
                     <div
-                        class="flex flex-wrap items-center justify-between">
+                            class="flex flex-wrap items-center justify-between">
                         <div class="w-full flex-grow sm:w-auto">
                             <a href="{{route('books.show', $book)}}" class="book-title">
                                 {{ $book->title }}
